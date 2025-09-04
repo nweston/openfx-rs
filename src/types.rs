3725c9e13d5d9ff59b50d357164ccf7572b5f47b
Nathan Weston <nweston@fastmail.com>
2025-09-04 15:44:59 -0400

    Export more types

--- src/types.rs
+++ src/types.rs
@@ -1,4 +1,7 @@
 pub use openfx_sys::{
-    OfxHost, OfxImageClipHandle, OfxImageEffectHandle, OfxParamHandle, OfxParamSetHandle,
-    OfxPropertySetHandle, OfxRectD, OfxRectI, OfxStatus, OfxTime,
+    OfxHost, OfxImageClipHandle, OfxImageEffectHandle, OfxImageMemoryHandle,
+    OfxMutexHandle, OfxParamHandle, OfxParamSetHandle, OfxPlugin, OfxPointD, OfxPointI,
+    OfxPropertySetHandle, OfxRGBAColourB, OfxRGBAColourD, OfxRGBAColourF, OfxRGBAColourS,
+    OfxRGBColourB, OfxRGBColourD, OfxRGBColourF, OfxRGBColourS, OfxRangeD, OfxRangeI,
+    OfxRectD, OfxRectI, OfxStatus, OfxTime,
 };
